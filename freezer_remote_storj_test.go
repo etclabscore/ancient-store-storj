310f05e7c22fbeaf1bb4e26a6c56bfa4c28b8334
Zane Starr <zcstarr@gmail.com>
2020-07-31 12:26:42 -0700

    freezer_remote_storj_test.go: refactor tests to one shot vs. run individual cases

--- freezer_remote_storj_test.go
+++ freezer_remote_storj_test.go
@@ -5,66 +5,34 @@ import (
 	"math/rand"
 	"os"
 	"os/exec"
-	"strings"
 	"testing"
+
+	"github.com/ethereum/go-ethereum/cmd/utils"
 )
 
 var (
-	apiKey    = ""
-	secret    = ""
-	satellite = "us-central-1.tardigrade.io:7777"
-	ipcPath   = os.TempDir() + "ancient.ipc"
+	ipcPath = os.TempDir() + "ancient.ipc"
 )
 
 func runMain(bucket string) {
-	os.Setenv("STORJ_API_KEY", apiKey)
-	os.Setenv("STORJ_SECRET", secret)
-	os.Setenv("STORJ_SATELLITE", satellite)
-
 	os.Args = append([]string{"./ancient-store-storj", "--bucket", fmt.Sprintf("unit-test-%v", rand.Int()), "--loglevel", "3", "--ipcpath", ipcPath})
 	main()
 
 }
 
-func getTestCases() []string {
-	testCmd := exec.Command("go", "test", "github.com/ethereum/go-ethereum/core", "-list", "_RemoteFreezer")
-	testCmd.Stderr = os.Stderr
-	output, err := testCmd.Output()
-	if err != nil {
-		panic(err)
-	}
-	cases := strings.Split(string(output), "\n")
-	parsedCases := []string{}
-	for _, c := range cases {
-		if strings.HasPrefix(c, "Test") {
-			parsedCases = append(parsedCases, c)
-		}
-	}
-	return parsedCases
-}
-
-func runTestCase(testCase string) {
-
+func TestIntegration(t *testing.T) {
+	defer os.RemoveAll(ipcPath)
+	defer stopServer()
 	go func() {
-		runMain(testCase)
+		runMain("etclabs-storj-freezer-test")
 	}()
-	testFilter := fmt.Sprintf("-run=%s", testCase)
-	fmt.Println("TESTCASE===================:", testCase)
-	testCmd := exec.Command("go", "test", "github.com/ethereum/go-ethereum/core", "-count=1", "-v", testFilter)
+	testCmd := exec.Command("go", "test", "github.com/ethereum/go-ethereum/core", "-count", "1", "-v", "-run", "_RemoteFreezer")
 	testCmd.Env = os.Environ()
 	testCmd.Env = append(os.Environ(), fmt.Sprintf("GETH_ANCIENT_RPC=%s", ipcPath))
 	testCmd.Stderr = os.Stderr
-	//	testCmd.Stdout = os.Stdout
+	testCmd.Stdout = os.Stdout
 	err := testCmd.Run()
 	if err != nil {
-		panic(err)
-	}
-}
-
-func TestIntegration(t *testing.T) {
-
-	testCases := getTestCases()
-	for _, testCase := range testCases {
-		runTestCase(testCase)
+		utils.Fatalf("Unexpected error", err)
 	}
 }
